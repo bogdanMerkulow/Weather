f791c05dbf4abeec4ae9394c2903b48a32d3d333
bogdanMerkulow <bogdanMerkulow@mail.ru>
2021-07-05 17:21:29 +0300

    Clean imports

--- app/src/main/java/com/example/application/adapters/WeatherDetailRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/application/adapters/WeatherDetailRecyclerViewAdapter.kt
@@ -1,14 +1,8 @@
 package com.example.application.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.example.application.R
 import com.example.application.models.Weather
 import com.example.application.viewholders.WeatherDetailViewHolder
 
